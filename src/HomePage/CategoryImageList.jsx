457765755942625ea68ee57475ae5a68183b8611
lavdimPireva <lavdimpireva2@gmail.com>
2023-11-21 21:01:42 +0100

    making hompage responsive for mobile and tablet screens

--- src/HomePage/CategoryImageList.jsx
+++ src/HomePage/CategoryImageList.jsx
@@ -1,15 +1,44 @@
-import React from "react";
+import React, { useRef } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
 
-const CategoryImageList = ({ images }) => (
-  <div className="horizontal-scroll-wrapper">
-    {images.map((image, index) => (
-      <div className="horizontal-scroll-item" key={index}>
-        <a href={image.link}>
-          <img src={image.src} alt={image.alt} className="category-image" />
-        </a>
+const CategoryImageList = ({ images }) => {
+  const scrollContainer = useRef(null);
+
+  const scrollLeft = () => {
+    if (scrollContainer.current) {
+      scrollContainer.current.scrollLeft -= 200;
+    }
+  };
+
+  const scrollRight = () => {
+    if (scrollContainer.current) {
+      scrollContainer.current.scrollLeft += 200;
+    }
+  };
+
+  return (
+    <>
+      <button onClick={scrollLeft} className="scroll-arrow left">
+        <FontAwesomeIcon icon={faChevronLeft} />
+      </button>
+      <div className="horizontal-scroll-wrapper" ref={scrollContainer}>
+        {images.map((image, index) => (
+          <div className="horizontal-scroll-item" key={index}>
+            <a href={image.link}>
+              <img src={image.src} alt={image.alt} className="category-image" />
+            </a>
+          </div>
+        ))}
       </div>
-    ))}
-  </div>
-);
+      <button onClick={scrollRight} className="scroll-arrow right">
+        <FontAwesomeIcon icon={faChevronRight} />
+      </button>
+    </>
+  );
+};
 
 export default CategoryImageList;
