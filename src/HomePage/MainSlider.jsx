5abd25ba8e8a359f02764114ca2156709b033ddc
lavdimPireva <lavdimpireva2@gmail.com>
2023-12-14 13:43:09 +0100

    commit

--- src/HomePage/MainSlider.jsx
+++ src/HomePage/MainSlider.jsx
@@ -7,7 +7,15 @@ const MainSlider = ({ settings, slides }) => (
   <Slider {...settings}>
     {slides.map((slide, index) => (
       <div key={index}>
-        <img src={slide.src} alt={slide.alt} className="responsive-image" />
+        <img
+          src={slide.src}
+          alt={slide.alt}
+          className="responsive-image"
+          onError={(e) => {
+            e.target.onerror = null; // Prevents infinite loop if fallback also fails
+            e.target.src = slide.fallback; // Use the fallback image URL
+          }}
+        />
       </div>
     ))}
   </Slider>
--- src/ImagesModule/mainSliderImages.js
+++ src/ImagesModule/mainSliderImages.js
@@ -1,14 +1,28 @@
-const img1 =
-  "https://atletjaime-images.s3.eu-central-1.amazonaws.com/slide1.avif";
-const img2 =
-  "https://atletjaime-images.s3.eu-central-1.amazonaws.com/slide2.avif";
-const img3 =
-  "https://atletjaime-images.s3.eu-central-1.amazonaws.com/slide3.avif";
+const img1 = {
+  src: "https://atletjaime-images.s3.eu-central-1.amazonaws.com/slide1.avif",
+  fallback:
+    "https://atletjaime-images.s3.eu-central-1.amazonaws.com/slide1.jpg",
+  alt: "Image1",
+};
+
+const img2 = {
+  src: "https://atletjaime-images.s3.eu-central-1.amazonaws.com/slide2.avif",
+  fallback:
+    "https://atletjaime-images.s3.eu-central-1.amazonaws.com/slide2.jpg",
+  alt: "Image1",
+};
+
+const img3 = {
+  src: "https://atletjaime-images.s3.eu-central-1.amazonaws.com/slide3.avif",
+  fallback:
+    "https://atletjaime-images.s3.eu-central-1.amazonaws.com/slide3.jpg",
+  alt: "Image1",
+};
 
 const mainSliderImages = [
-  { src: img1, alt: "Image1" },
-  { src: img2, alt: "Image2" },
-  { src: img3, alt: "Image3" },
+  { src: img1.src, fallback: img1.fallback, alt: img1.alt },
+  { src: img2.src, fallback: img2.fallback, alt: img2.alt },
+  { src: img3.src, fallback: img3.fallback, alt: img3.alt },
 ];
 
 export default mainSliderImages;
